bef1ae07856d08bd908813335f6ca74ef4bf32ea
why-me1 <arifulislammahim539@gmail.com>
2023-08-22 18:42:59 +0600

    via upload

---
+++ Codeforces/Educational/A_Not_a_Substring.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define ld long double
+#define nl "\n"
+#define ff first
+#define ss second
+#define pb push_back
+#define pf push_front
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define unique(x) x.erase(unique(all(x)), x.end());
+#define set_bits(x) __builtin_popcountll(x)
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+int main(){
+ios_base::sync_with_stdio(false);
+cin.tie(nullptr);
+int t; cin >> t; while(t--){
+string s,a="",b="",ans;
+cin>>s;
+int n=s.size();
+if(s=="()"){
+    no;
+    continue;
+}
+for(int i=0;i<n;i++)a+='(';
+for(int i=0;i<n;i++)a+=')';
+for(int i=0;i<n;i++){
+    b = b + '(' + ')';
+}
+size_t f= a.find(s);
+if(f!=string :: npos)ans=b;
+else ans=a;
+yes;
+cout<<ans<<nl;
+}
+return 0;
+}
